084ee979bed4c57f44930fd86e2e38fdbd18edbb
Alejandra M. Saucedo <arqtech.meteo@gmail.com>
2024-04-18 12:43:47 +0200

    Test Temps Calcul

--- Ressources/Ressources/temps_de_calcul.c
+++ Ressources/Ressources/temps_de_calcul.c
@@ -114,22 +114,45 @@ int main(int argc,char** argv){
     double temps_lc_debut;
     double temps_lc_fin;
 
-    double temps_h = 0;
-    double temps_h_debut;
-    double temps_h_fin;
+    double temps_h1 = 0;
+    double temps_h1_debut;
+    double temps_h1_fin;
+
+    double temps_h2 = 0;
+    double temps_h2_debut;
+    double temps_h2_fin;
+
+    double temps_h3 = 0;
+    double temps_h3_debut;
+    double temps_h3_fin;
+
+    double temps_h4 = 0;
+    double temps_h4_debut;
+    double temps_h4_fin;
+
+    double temps_h5 = 0;
+    double temps_h5_debut;
+    double temps_h5_fin;
 
     double temps_a = 0;
     double temps_a_debut;
     double temps_a_fin; 
 
-    FILE *f2 = fopen("temps_calcul_lc.txt", "w");
-    FILE *f3 = fopen("temps_calcul_ha.txt","w"); 
-    //Taille de hachage : 10, 50, 100, 500, 1000
+    FILE *f2 = fopen("temps_calcul_lc_2.txt", "w");
+    FILE *f3 = fopen("temps_calcul_ha_2.txt","w"); 
 
-    for(int nbChaines=500; nbChaines<=5000; nbChaines+=500){
+    for(int nbChaines=2000; nbChaines<=3500; nbChaines+=500){
+        //1500 premier tour
         //int nbChaines, int nbPointsChaine, int xmax,int ymax
         Chaines* chaine = generationAleatoire(nbChaines,100,5000,5000);
-        int M = 10;  
+
+        //Taille de hachage : 10, 50, 100, 500, 1000
+        int M1 = 10;  
+        int M2 = 50;
+        int M3 = 100;
+        int M4 = 500;
+        int M5 = 1000; 
+
 
         //ecrireChaines(chaine,f2);
 
@@ -137,21 +160,41 @@ int main(int argc,char** argv){
         reconstitueReseauListe(chaine);
         temps_lc_fin = clock();
 
-        temps_h_debut = clock();
-        reconstitueReseauHachage(chaine,M);
-        temps_h_fin = clock();
+        temps_h1_debut = clock();
+        reconstitueReseauHachage(chaine,M1);
+        temps_h1_fin = clock();
+
+        temps_h2_debut = clock();
+        reconstitueReseauHachage(chaine,M2);
+        temps_h2_fin = clock();
+
+        temps_h3_debut = clock();
+        reconstitueReseauHachage(chaine,M3);
+        temps_h3_fin = clock();
+
+        temps_h4_debut = clock();
+        reconstitueReseauHachage(chaine,M4);
+        temps_h4_fin = clock();
+
+        temps_h5_debut = clock();
+        reconstitueReseauHachage(chaine,M5);
+        temps_h5_fin = clock();
 
         temps_a_debut = clock();
         reconstitueReseauArbre(chaine);
         temps_a_fin = clock(); 
 
         temps_lc = ((double)(temps_lc_fin-temps_lc_debut))/CLOCKS_PER_SEC;
-	    temps_h = ((double)(temps_h_fin-temps_h_debut))/CLOCKS_PER_SEC;
+	    temps_h1 = ((double)(temps_h1_fin-temps_h1_debut))/CLOCKS_PER_SEC;
+        temps_h2 = ((double)(temps_h2_fin-temps_h2_debut))/CLOCKS_PER_SEC;
+        temps_h3 = ((double)(temps_h3_fin-temps_h3_debut))/CLOCKS_PER_SEC;
+        temps_h4 = ((double)(temps_h4_fin-temps_h4_debut))/CLOCKS_PER_SEC;
+        temps_h5 = ((double)(temps_h5_fin-temps_h5_debut))/CLOCKS_PER_SEC;
         temps_a = ((double)(temps_a_fin-temps_a_debut))/CLOCKS_PER_SEC;
 
         //fprintf(f2,"Temps de calcul pour le fichier %s\n","test_graphe.txt");
         fprintf(f2,"%d %lf\n",nbChaines, temps_lc);
-        fprintf(f3,"%d %lf %lf\n",nbChaines, temps_h, temps_a);
+        fprintf(f3,"%d %lf %lf %lf %lf %lf %lf\n",nbChaines, temps_h1, temps_h2, temps_h3, temps_h4, temps_h5,temps_a);
 
         printf("%d\n", nbChaines); 
         //liberer_Chaines(chaine);
